287b63964532a64f347224d290094376cdca8651
adhi0331 <adhi03.ananthan@gmail.com>
2022-05-01 12:32:31 -0700

    Added tests and method

--- skilldemo.java
+++ skilldemo.java
@@ -1,3 +1,7 @@
 public class skilldemo {
 
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+
 }
---
+++ skilldemotester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skilldemotester {
+    
+    @Test
+    public void subtract(){
+        assertEquals(skilldemo.subtract(2, 1), 0);
+    }
+}
